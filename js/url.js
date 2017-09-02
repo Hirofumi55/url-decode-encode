9aede9a31ba881331a7d09eaa5f8ae8bee196047
Hirofumi <sasa1235711@gmail.com>
2017-09-03 00:07:02 +0900

    Adsense削除

--- js/url.js
+++ js/url.js
@@ -4,23 +4,23 @@
 
 
 //Encode URLがクリックで実行されるメソッド
-$('#encode-button').on('click', function() {
+$('#encode-button').on('click', function () {
 
     //URLフォームから値を取得
     var URL = $('#URLForm').val()
     console.log(URL)
-    
+
     //URLエンコード
     var encodeData = encodeURI(URL)
     console.log(encodeData)
 
     //Encoded URLの値を設定
     $('#encodedURL-text').val(encodeData);
-    });
+});
 
 
 //Decode URLがクリックで実行されるメソッド
-$('#decode-button').on('click', function() {
+$('#decode-button').on('click', function () {
 
     //URLフォームから値を取得
     var URL = $('#URLForm').val()
@@ -31,10 +31,10 @@ $('#decode-button').on('click', function() {
     console.log(decodeData)
 
     //Decoded URLの値を設定
-    $('#decodedURL-text').val(decodeData);
+    $('#decodedURL-text').val(decodeData)
 });
 
-$('#super-button').on('click', function() {
+$('#super-button').on('click', function () {
 
     //URLフォームから値を取得
     var URL = $('#URLForm').val()
@@ -52,5 +52,16 @@ $('#super-button').on('click', function() {
     console.log(encodeData)
 
     //Encoded URLの値を設定
-    $('#encodedURL-text').val(encodeData);
+    $('#encodedURL-text').val(encodeData)
 });
+
+$('#clear-button').on('click', function () {
+
+    //アラートを表示
+    alert("フォームを削除してよろしいですか？")
+    //テキストボックスをすべて空にする
+    $('#URLForm').val("")
+    $('#encodedURL-text').val("")
+    $('#decodedURL-text').val("")
+
+})
